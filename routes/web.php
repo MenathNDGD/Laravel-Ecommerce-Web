4b8892f3bd96a447152ba3532b6d676e1baec2c2
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-03 01:27:28 +0530

    added a new route for the category tab

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,7 @@ Route::middleware([
 });
 
 route::get('/redirect', [HomeController::class, 'redirect']);
+
+route::get('/view_category', [AdminController::class, 'view_category']);
+
+route::post('/add_category', [AdminController::class, 'add_category']);
