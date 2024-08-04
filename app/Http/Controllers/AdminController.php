739964fe1c6879f0b31e513991884e1ea852f928
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-04 23:49:33 +0530

    added product view functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function view_category()
     {
-        return view('admin.category');
+        $data = Category::all();
+
+        return view('admin.category', compact('data'));
     }
 
     public function add_category(Request $request)
@@ -21,5 +24,42 @@ class AdminController extends Controller
         $data->save();
 
         return redirect()->back()->with('message', 'Category Added Successfully!');
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+
+        $data->delete();
+        
+        return redirect()->back()->with('message', 'Category Deleted Successfully');
+    }
+
+    public function add_product()
+    {
+        $category = Category::all();
+
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product_items(Request $request)
+    {
+        $product = new Product();
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->discount_price = $request->discount_price;
+        $product->quantity = $request->quantity;
+        $product->category = $request->category;
+
+        $image = $request->image;
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product', $imagename);
+        $product->image = $imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Added Successfully');
     }
 }
