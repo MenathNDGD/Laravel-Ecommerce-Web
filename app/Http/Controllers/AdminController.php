5fa79abd6b6ae212cad9b76c4bfb15f581b7333b
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-06 22:27:59 +0530

    added new admin controll fuctions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -62,4 +62,53 @@ class AdminController extends Controller
 
         return redirect()->back()->with('message', 'Product Added Successfully');
     }
+
+    public function view_product()
+    {
+        $product = Product::all();
+
+        return view('admin.view_product', compact('product'));
+    }
+
+    public function delete_product($id)
+    {
+        $product = Product::find($id);
+
+        $product->delete();
+
+        return redirect()->back()->with('message', 'Product Item Deleted Successfully');
+    }
+
+    public function update_product($id)
+    {
+        $product = Product::find($id);
+
+        $category = Category::all();
+
+        return view('admin.update_product', compact('product', 'category'));
+    }
+
+    public function update_product_confirm(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->discount_price = $request->discount_price;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+
+        $image = $request->image;
+        if ($image)
+        {
+            $imagename = time().'.'.$image->getClientOriginalExtension();
+            $request->image->move('product', $imagename);
+            $product->image = $imagename;
+        }
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Update Successfully');
+    }
 }
