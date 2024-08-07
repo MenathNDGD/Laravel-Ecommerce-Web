abc6792da102433f8f32f975d1c1d0a61e542663
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-08 02:29:24 +0530

    added new routes for newly created functions

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ Route::middleware([
     })->name('dashboard');
 });
 
-route::get('/redirect', [HomeController::class, 'redirect']);
-
+# Admin Routes
 route::get('/view_category', [AdminController::class, 'view_category']);
 
 route::post('/add_category', [AdminController::class, 'add_category']);
@@ -35,6 +34,14 @@ route::get('/delete_product/{id}', [AdminController::class, 'delete_product']);
 route::get('/update_product/{id}', [AdminController::class, 'update_product']);
 
 route::post('/update_product_confirm/{id}', [AdminController::class, 'update_product_confirm']);
+
+route::get('/view_orders', [AdminController::class, 'view_orders']);
+
+route::get('/delivered/{id}', [AdminController::class, 'delivered']);
+
+
+# Home Routes
+route::get('/redirect', [HomeController::class, 'redirect']);
 
 route::get('/product_details/{id}', [HomeController::class, 'product_details']);
 
