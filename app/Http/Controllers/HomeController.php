9419f8a5d685bd19b75670ed20d177eb7ce60a2d
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-10 03:41:09 +0530

    added new controllers for home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,4 +32,11 @@ class HomeController extends Controller
             return view('home.userpage', compact('product'));
         }
     }
+
+    public function product_details($id)
+    {
+        $product = Product::find($id);
+
+        return view('home.product_details', compact('product'));
+    }
 }
