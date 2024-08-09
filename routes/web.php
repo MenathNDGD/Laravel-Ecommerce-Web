afacadfef9b06022c5e39afbd2c3c40a1ceab366
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-10 00:16:31 +0530

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+# Navigation Routes
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -12,6 +14,26 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/products', function () {
+    return view('product.body');
+});
+
+Route::get('/blog', function () {
+    return view('blog.body');
+});
+
+Route::get('/contact', function () {
+    return view('contact.body');
+});
+
+Route::get('/about', function () {
+    return view('about.body');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial.body');
 });
 
 # Home Controller Routes
@@ -37,3 +59,6 @@ route::get('/delete_product/{id}', [AdminController::class, 'delete_product']);
 route::get('/update_product/{id}', [AdminController::class, 'update_product']);
 
 route::post('/update_product_confirm/{id}', [AdminController::class, 'update_product_confirm']);
+
+# Product Controller Routes
+Route::get('/products', [ProductController::class, 'showAllProducts']);
