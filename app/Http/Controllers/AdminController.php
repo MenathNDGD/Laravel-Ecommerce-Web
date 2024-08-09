432cc4d486bfbb71bb90fdd1ae3936b3a74b34a5
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-09 13:42:16 +0530

    created new admin controllers for adding products

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -32,5 +33,33 @@ class AdminController extends Controller
         $data->delete();
         
         return redirect()->back()->with('message', 'Category Deleted Successfully');
+    }
+
+    public function add_product()
+    {
+        $category = Category::all();
+
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product_items(Request $request)
+    {
+        $product = new Product();
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->discount_price = $request->discount_price;
+        $product->quantity = $request->quantity;
+        $product->category = $request->category;
+
+        $image = $request->image;
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product', $imagename);
+        $product->image = $imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Added Successfully');
     }
 }
