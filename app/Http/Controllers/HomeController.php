15d2b8c159e8f019cb35c0a54e96f68b85db02e9
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-10 00:14:12 +0530

    created new controllers for home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -10,7 +11,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.userpage');
+        $product = Product::paginate(3);
+
+        return view('home.userpage', compact('product'));
     }
 
     public function redirect()
@@ -20,10 +23,13 @@ class HomeController extends Controller
         if ($usertype == '1')
         {
             return view('admin.home');
-        }
-        else
+
+        } 
+        else 
         {
-            return view('home.userpage');
+            $product = Product::paginate(3);
+
+            return view('home.userpage', compact('product'));
         }
     }
 }
