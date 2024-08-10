92ebac507555765ada96af1fd6937ff6e556a5c1
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-10 15:24:11 +0530

    created new product controller functions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,6 +13,19 @@ class ProductController extends Controller
     {
         $product = Product::all();
 
-        return view('product.body', compact('product'));
+        if (Auth::id())
+        {
+            $id = Auth::user()->id;
+
+            $cart = Cart::where('user_id', '=', $id)->get();
+
+            $cartCount = $cart->count();
+
+            return view('product.body', compact('product', 'cartCount'));
+        }
+        else
+        {
+            return view('product.body', compact('product'));
+        }
     }
 }
