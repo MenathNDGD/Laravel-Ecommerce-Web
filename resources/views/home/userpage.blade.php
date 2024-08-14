63c75c6e5bbf25117f7874b7a50a80452c484796
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-15 00:43:16 +0530

    included the sweet alert cdn

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -24,6 +24,9 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
    </head>
    <body>
+      <!-- sweet alert strats -->
+      @include('sweetalert::alert')
+      <!-- sweet alert ends -->
       <div class="hero_area">
          <!-- header section strats -->
          @include('home.header')
