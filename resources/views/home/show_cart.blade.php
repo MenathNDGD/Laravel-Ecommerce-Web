57b608a19bcb6799398cf1c4f3ad51d87cb45a5a
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-14 09:43:16 +0530

    modified the cart page with new styles

--- resources/views/home/show_cart.blade.php
+++ resources/views/home/show_cart.blade.php
@@ -120,13 +120,59 @@
     }
 
     .nav-link {
-        display: flex;
-        align-items: center;
-    }
+      display: flex;
+      align-items: center;
+   }
 
-    .badge {
-        margin-left: 5px;
-    }
+   .badge {
+      margin-left: 5px;
+   }
+
+   .nav-item .nav-link {
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+   }
+
+   .nav-item .nav-link:hover {
+      color: #d4e157;
+   }
+
+   .nav-item.dropdown {
+      position: relative;
+   }
+
+   .userDropdown {
+      border: 2px solid #dbd5d5;
+      margin-left: 20px;
+   }
+
+   .dropdown-menu {
+      display: none;
+      position: absolute;
+      top: 100%;
+      right: 0;
+      background-color: #ffffff;
+      border: 1px solid #ddd;
+      border-radius: 0.25rem;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+      z-index: 1000;
+   }
+
+   .dropdown-menu .dropdown-item {
+      padding: 10px 15px;
+      color: #333;
+      text-decoration: none;
+      display: block;
+   }
+
+   .dropdown-menu .dropdown-item:hover {
+      background-color: #f1f1f1;
+   }
+
+   .nav-item.dropdown.show .dropdown-menu {
+      display: block;
+   }
    </style>
 </head>
 <body>
@@ -167,7 +213,10 @@
                             <span class="badge badge-pill badge-dark">{{ $cartCount }}</span>
                           @endif
                         </a>
-                    </li>                     
+                    </li>
+                    <li class="nav-item">
+                     <a class="nav-link" href="{{ url('/my_orders') }}">Orders</a>
+                   </li>                     
                     <form class="form-inline">
                       <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                       <i class="fa fa-search" aria-hidden="true"></i>
@@ -176,9 +225,22 @@
     
                     @if (Route::has('login'))
                       @auth
-                         <li class="nav-item">
-                            <x-app-layout></x-app-layout>
-                         </li>
+                        <li class="nav-item dropdown">
+                           <a id="navbarDropdown" class="nav-link dropdown-toggle userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              {{ Auth::user()->name }}
+                           </a>
+                           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ url('/dashboard') }}">
+                                 Profile
+                              </a>
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
+                                 @csrf
+                                 <button type="submit" class="dropdown-item">
+                                    Logout
+                                 </button>
+                              </form>
+                           </div>
+                        </li>
                       @else
                          <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
